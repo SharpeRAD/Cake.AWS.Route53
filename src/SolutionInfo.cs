4e00ac9f1e70444ef6cd6d39ab8122f2a58bd793
SharpeRAD <code@sharperad.com>
2016-01-07 21:25:13 +0000

    Update Solution Info

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,9 +5,9 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyProduct("Cake.AWS.S3")]
-[assembly: AssemblyVersion("0.0.8")]
-[assembly: AssemblyFileVersion("0.0.8")]
-[assembly: AssemblyInformationalVersion("0.0.8")]
+[assembly: AssemblyProduct("Cake.AWS.Route53")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
 [assembly: AssemblyCopyright("Copyright (c) Phillip Sharpe 2015")]
 
