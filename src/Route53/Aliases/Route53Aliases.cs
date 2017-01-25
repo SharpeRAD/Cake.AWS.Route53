63b7a2425372af456ba823a0dfd17e5a81198bd6
Cunningham, Andrew (Seattle) <andrew.cunningham@us.imshealth.com>
2017-01-24 19:45:09 -0800

    Add necessary parameters to DeleteResourceRecordSet

--- src/Route53/Aliases/Route53Aliases.cs
+++ src/Route53/Aliases/Route53Aliases.cs
@@ -31,7 +31,7 @@ namespace Cake.AWS.Route53
 
 
         /// <summary>
-        /// Create a new hosted zone. When you create a zone, its initial status is PENDING. This means that it is not yet available on all DNS servers. 
+        /// Create a new hosted zone. When you create a zone, its initial status is PENDING. This means that it is not yet available on all DNS servers.
         /// The status of the zone changes to INSYNC when the NS and SOA records are available on all Route 53 DNS servers.
         /// </summary>
         /// <param name="context">The cake context.</param>
@@ -45,7 +45,7 @@ namespace Cake.AWS.Route53
         }
 
         /// <summary>
-        /// Create a new hosted zone. When you create a zone, its initial status is PENDING. This means that it is not yet available on all DNS servers. 
+        /// Create a new hosted zone. When you create a zone, its initial status is PENDING. This means that it is not yet available on all DNS servers.
         /// The status of the zone changes to INSYNC when the NS and SOA records are available on all Route 53 DNS servers.
         /// </summary>
         /// <param name="context">The cake context.</param>
@@ -124,7 +124,7 @@ namespace Cake.AWS.Route53
         /// <param name="hostedZoneId">The ID of the hosted zone that contains the resource record sets that you want to change</param>
         /// <param name="name">The name of the DNS record set.</param>
         /// <param name="type">The type of the DNS record set.</param>
-        /// <param name="value">The value of the record set.</param> 
+        /// <param name="value">The value of the record set.</param>
         /// <param name="settings">The <see cref="Route53Settings"/> required to upload to Amazon S3.</param>
         [CakeMethodAlias]
         [CakeAliasCategory("Route53")]
@@ -140,7 +140,7 @@ namespace Cake.AWS.Route53
         /// <param name="hostedZoneId">The ID of the hosted zone that contains the resource record sets that you want to change</param>
         /// <param name="name">The name of the DNS record set.</param>
         /// <param name="type">The type of the DNS record set.</param>
-        /// <param name="value">The value of the record set.</param> 
+        /// <param name="value">The value of the record set.</param>
         /// <param name="ttl">The time to live of the record set.</param>
         /// <param name="settings">The <see cref="Route53Settings"/> required to upload to Amazon S3.</param>
         [CakeMethodAlias]
@@ -157,12 +157,14 @@ namespace Cake.AWS.Route53
         /// <param name="hostedZoneId">The ID of the hosted zone that contains the resource record sets that you want to change</param>
         /// <param name="name">The name of the DNS record set.</param>
         /// <param name="type">The type of the DNS record set.</param>
+        /// <param name="value">The value of the record set.</param>
+        /// <param name="ttl">The time to live of the record set.</param>
         /// <param name="settings">The <see cref="Route53Settings"/> required to upload to Amazon S3.</param>
         [CakeMethodAlias]
         [CakeAliasCategory("Route53")]
-        public static string DeleteResourceRecordSet(this ICakeContext context, string hostedZoneId, string name, RRType type, Route53Settings settings)
+        public static string DeleteResourceRecordSet(this ICakeContext context, string hostedZoneId, string name, RRType type, string value, long ttl, Route53Settings settings)
         {
-            return context.CreateManager().DeleteResourceRecordSet(hostedZoneId, name, type, settings);
+            return context.CreateManager().DeleteResourceRecordSet(hostedZoneId, name, type, value, ttl, settings);
         }
 
         /// <summary>
