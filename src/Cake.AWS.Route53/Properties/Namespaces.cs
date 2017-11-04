19c5993ce6d0e6091e4dbb5fec7ee998d576f6ef
SharpeRAD <code@sharperad.com>
2017-11-04 14:12:51 +0000

    Build scripts

---
+++ src/Cake.AWS.Route53/Properties/Namespaces.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+
+
+namespace Cake.AWS.Route53
+{
+    /// <summary>
+    /// This namespace contains Amazon Simple Storage Service aliases and related members.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+
+    }
+}
--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,9 +5,8 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyProduct("Cake.AWS.Route53")]
-[assembly: AssemblyVersion("0.0.6")]
-[assembly: AssemblyFileVersion("0.0.6")]
-[assembly: AssemblyInformationalVersion("0.0.6")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 - 2017 Phillip Sharpe")]
 
